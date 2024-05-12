dc14e05ca88591cdbc3652bb7096388ab29c4fc9
KMD <KMD@d.c>
2024-05-12 18:30:53 +0300

    init commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import './utils/vh.utils.js';
+import './utils/animate-on-view.utils'
+
+import './components/uploadFile.component.js';
+import './components/inputs.component.js';
+
+import './libs/countUp.lib.js';
+
